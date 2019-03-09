8ab9fb1dc5a98a5b9dd41d28fdf43fa35aef5054
kido050313 <1059064141@qq.com>
2019-03-09 21:19:02 +0800

    update

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -21,3 +21,34 @@ export function getData(el, name, val) {
     return el.getAttribute(prefix + name)
   }
 }
+
+let elmentStyle = document.createElement('div').style
+
+let vender = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elmentStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vender === false) {
+    return false
+  }
+  if (vender === 'standard') {
+    return style
+  }
+
+  return vender + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -15,7 +21,10 @@
             :probe-type="3"
             :listen-scroll="listenScroll">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list  @select="selectItem" :songs="songs"></song-list>
+      </div>
+      <div class="loading-container">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -23,14 +32,20 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import Loading from 'base/loading/loading'
+import {prefixStyle} from 'common/js/dom'
+import {mapActions} from 'vuex'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   name: 'musicList',
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   props: {
     bgImage: {
@@ -68,33 +83,47 @@ export default {
   methods: {
     scroll(pos) {
       this.scrollY = pos.y
-    }
+    },
+    back() {
+      this.$router.back()
+    },
+    selectItem(item, index) {
+      this.selectPlay({
+        list: this.songs,
+        index
+      })
+    },
+    ...mapActions([
+      'selectPlay'
+    ])
   },
   watch: {
-    scrollY(newY) {
+    scrollY(newVal) {
       let zIndex = 0
       let scale = 1
       let blur = 0
-      let translateY = Math.max(this.miniTranslateY, newY)
-      let percent = Math.abs(newY / this.imageHeight)
-      this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-      this.$refs.layer.style['-webkit-transform'] = `translate3d(0, ${translateY}px, 0)`
-      if (newY > 0) {
+      let translateY = Math.max(this.miniTranslateY, newVal)
+      let percent = Math.abs(newVal / this.imageHeight)
+      if (newVal > 0) {
         scale = 1 + percent
         zIndex = 10
       } else {
         blur = Math.min(20, percent * 20)
       }
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-      if (newY < this.miniTranslateY) {
+
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      if (newVal < this.miniTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
       this.$refs.bgImage.style.zIndex = zIndex
     }
   }
@@ -140,6 +169,30 @@ export default {
       padding-top 70%
       transform-origin top
       background-size cover
+      .play-wrapper
+        position absolute
+        bottom 20px
+        z-index 50
+        width 100%
+        .play
+          box-sizing border-box
+          width 135px
+          padding 7px 0
+          margin 0 auto
+          text-align center
+          border 1px solid $color-theme
+          color $color-theme
+          border-radius 100px
+          font-size 0
+          .icon-play
+            display inline-block
+            vertical-align middle
+            margin-right 6px
+            font-size $font-size-medium-x
+          .text
+            display inline-block
+            vertical-align middle
+            font-size $font-size-small
       .filter
         position absolute
         top 0
